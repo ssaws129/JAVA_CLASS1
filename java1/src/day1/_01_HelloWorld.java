f12c045bb491996da783939d400db4e5cf5934ec
wkdqk <wkdqk@192.168.0.11>
2025-02-09 22:08:29 +0900

    day1 git test

--- java1/src/day1/_01_HelloWorld.java
+++ java1/src/day1/_01_HelloWorld.java
@@ -5,6 +5,7 @@ public class _01_HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("안녕하세요:");
+		System.out.println("git test");
 	}
 
 }
